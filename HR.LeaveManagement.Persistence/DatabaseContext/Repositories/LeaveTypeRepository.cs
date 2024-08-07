153ed58a1a801afc033e8452d6fa6bf853bea7ab
marc <marc@LEEPE-MARC>
2024-08-07 20:33:53 +0800

    database persistence

---
+++ HR.LeaveManagement.Persistence/DatabaseContext/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,23 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        public LeaveTypeRepository(HRDatabaseContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> IsLeaveTypeUnique(string name)
+        {
+            return await _context.LeaveTypes.AnyAsync(q => q.Name == name);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using HR.LeaveManagement.Persistence.DatabaseContext.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<HRDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HRDatabaseConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
