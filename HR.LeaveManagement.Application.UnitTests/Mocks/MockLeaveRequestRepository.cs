7e3a75068c2ed00d867eb1cd1507bbb3cb1b40e6
marc <marc@LEEPE-MARC>
2024-08-17 13:58:52 +0800

    blazor ui

---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveRequestRepository.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks
+{
+    public class MockLeaveRequestRepository
+    {
+    }
+}
